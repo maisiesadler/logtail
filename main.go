d28d3b7b230b1a011f3fc108c63e94154721ca34
Maisie Sadler <maisiesadler@Maisies-MacBook-Pro.local>
2019-03-24 09:26:50 +0000

    new channel writer fn

--- main.go
+++ main.go
@@ -71,6 +71,10 @@ type ChannelWriter struct {
   updates chan string
 }
 
+func NewChannelWriter() *ChannelWriter {
+  return &ChannelWriter{ make(chan string) }
+}
+
 func (w *ChannelWriter) Write(data []byte) (n int, err error) {
   w.updates <- string(data)
   return 0, nil
