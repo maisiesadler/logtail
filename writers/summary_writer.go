2c27f2bafd258320af3e7a457e104a7986bb65bb
Maisie Sadler <maisiesadler@Maisies-MacBook-Pro.local>
2019-03-27 19:27:16 +0000

    make Read return array instead of chan

---
+++ writers/summary_writer.go
@@ -0,0 +1,58 @@
+package writers
+
+import (
+  "sync"
+  "regexp"
+)
+
+type SummaryWriter struct {
+  updates chan bool
+  Summary *Summary
+}
+
+func NewSummaryWriter() *SummaryWriter {
+  summary := &Summary{linecount: make(map[string]int)}
+  return &SummaryWriter{make(chan bool), summary}
+}
+
+func (w *SummaryWriter) Write(data []byte) (n int, err error) {
+  if added := w.Summary.Add(string(data)); added {
+    w.updates <- true
+  }
+  return 0, nil
+}
+
+func (w *SummaryWriter) Updates() <-chan bool {
+  return w.updates
+}
+
+type Summary struct {
+  sync.RWMutex
+  linecount map[string]int
+}
+
+type LineAndCount struct {
+  Line string
+  Count int
+}
+
+func (s *Summary) Add(l string) bool {
+  if matched, _ := regexp.MatchString("testing122", l); matched {
+    s.Lock()
+    s.linecount[l]++
+    s.Unlock()
+    return true
+  }
+  return false
+}
+
+func (s *Summary) Read() []LineAndCount {
+  lac := []LineAndCount{}
+  s.RLock()
+  for k, v := range s.linecount {
+    lac = append(lac, LineAndCount { k, v })
+  }
+  s.RUnlock()
+  return lac
+}
+
